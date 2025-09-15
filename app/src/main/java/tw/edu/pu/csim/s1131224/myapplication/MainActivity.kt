291be7370f5df5c782f50b52c88c76fe501aae87
s1131224 <lekent1992@gmail.com>
2025-09-15 17:08:03 +0800

    第六次commit (狀態管理)

--- app/src/main/java/tw/edu/pu/csim/s1131224/myapplication/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/s1131224/myapplication/MainActivity.kt
@@ -1,5 +1,24 @@
 package tw.edu.pu.csim.s1131224.myapplication
 
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import tw.edu.pu.csim.s1131224.myapplication.ui.theme.LottoTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,23 +38,22 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Play(modifier: Modifier = Modifier) {
-
-    var lucky = (1..100).random()
+    // 使用 remember 和 mutableStateOf 來記住並更新樂透數字
+    var lucky by remember { mutableStateOf((1..100).random()) }
 
     Box(
-        modifier = modifier.fillMaxSize(), // 讓 Box 填滿整個螢幕
-        contentAlignment = Alignment.Center // 水平與垂直置中
+        modifier = modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center // 水平垂直置中
     ) {
         Column(horizontalAlignment = Alignment.CenterHorizontally) {
             Text(
                 text = "樂透數字(1-100)為 $lucky"
             )
             Button(
-                onClick = { lucky = (1..100).random() }
+                onClick = { lucky = (1..100).random() } // 按鈕按下時更新 lucky 值
             ) {
                 Text("重新產生樂透碼")
             }
         }
     }
 }
-
